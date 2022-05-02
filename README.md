3ce752ba60196a734abeba676aad3da6c881295e
Waqas Naveed <waqas.naveed@gmail.com>
2022-05-02 04:20:42 +0000

    update readme file and util function

--- README.md
+++ README.md
@@ -1,22 +1,25 @@
 ## Shortlyster - SignUp
 
 ## Framework
-* Demonstrate your skill at coding automation frameworks for use by other engineers.
-* Demonstrate your ability to advise others on writing valuable automated tests.
+* I have used
+  * Language - python
+  * framework - pytest
+  * UI interaction - page objects (xpath / css selectors)
+  * Browser loading - seleniun webdriver
 
+ The reason of using is because I have started learning Python this year and I am still new with it. Every testcase will be a pytest function which will look for page objects. In side the page objects, I have used selenium function e.g. clicks, submit etc.... which interact with browser using webdriver. 
+ 
 ## Acceptance criteria
+I wrote down in the Gerkin language (Given When Then)
 As a user of shortlyster\
 I want to upload the curriculum vitae in the profile page\
 So that the document data is pre-fill on Experience & Skills page. 
 
-* To help them, write some example automated tests to verify a candidate can upload their curriculum vitae (CV) in the Profile page and the data captured is used to pre-fill some fields on the Experience and Skills page.
 
 ## Details
-* Use any testing framework you are comfortable with, explaining your choice.
-* The majority of our engineering is done in TypeScript, this is preferred, but not a requirement.
+* I couldn't get around the very first step of logging in to the website. I reckon the bot/automation is being blocked by the CDN or the network Shortlyster is currently using. I have tried so many ways to get through this but miserably failed. I have even tried Gecko driver (firefox) to change the header info but still couldn't make it.
+  
+* I partially went through the upload process and still needs to add assertions.
+* I planned to set up the classes but since it's a single feature so though about creating a main file along with utilities. The other way is to create a single class to initiate the objects and then subsequently create the methods to expose their behaviour. 
 
-## Submitting your results
-Once you’ve written your tests, run them successfully and are happy with the result:
-* Document any test prerequisites, assumptions made and any issues found a README file.
-* Push your files up to a GitHub repository and share your repository with us. Alternatively email us a Zip file with all
-required files
+
---
+++ Utilities.py
@@ -0,0 +1,92 @@
+# This is a Python script for Shortlyster CV upload automation.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from time import sleep
+from webbrowser import Chrome
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import TimeoutException
+from webdriver_manager import driver
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC, wait
+from selenium.common.exceptions import NoSuchElementException
+
+
+def webdriver_load():
+    chrome_options = webdriver.chrome.options.Options()
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_argument("--incognito")
+
+    chrome_options.add_argument('start-maximized')
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_argument('--start-maximized')
+    chrome_options.add_argument('--disable-infobars')
+
+    ser = Service("C:\DRIVERS\chromedriver_win32\chromedriver.exe")
+    # driver = webdriver.Chrome(options=chrome_options, executable_path=r"C:\DRIVERS\chromedriver_win32\chromedriver.exe")
+    driver = webdriver.Chrome(service=ser, options=chrome_options, executable_path=ser)
+
+    # yield driver  #return the browser object in case other tests wants to call it.
+    driver.get("https://candidate--qa-exercise-april.reviews.compono.dev/signup")
+    wait = WebDriverWait(driver, 10)
+
+    username = wait.until(EC.element_to_be_clickable((By.ID, '1-email')))
+    username.click()
+    username.send_keys('waqas.naveed@gmail.com')
+    password_field = wait.until(EC.element_to_be_clickable((By.NAME, 'password')))
+    password_field.click()
+    password_field.send_keys('@Shortly@123')
+    # wait
+    # username = driver.find_element_by_xpath('//*[@id="1-email"]')
+    # password = driver.find_element_by_xpath('//*[@id="auth0-lock-container-1"]/div/div[2]/form/div/div/div/div/div[2]/div[2]/span/div/div/div/div/div/div/div/div/div[2]/div/div[2]/div/div/input')
+    #
+    # username.click()
+    # username.send_keys("waqas.naveed@gmail.com")
+    # password.click()
+    # password.send_keys("@Shortly@123")
+
+    loginButton = driver.find_element_by_xpath('//*[@id="auth0-lock-container-1"]/div/div[2]/form/div/div/div/button')
+    loginButton = driver.find_element(by=By.XPATH,value='//*[@id="auth0-lock-container-1"]/div/div[2]/form/div/div/div/button')
+    loginButton.click()
+
+    # try:
+    #     element = driver.find_element_by_partial_link_text("WRONG EMAIL OR PASSWORD.")
+    # except NoSuchElementException:
+    #     print("No element found")
+
+
+def is_element_exist(text):
+    try:
+        # validationText = wait.until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, text)))
+        # '//*[@id="auth0-lock-container-1"]/div/div[2]/form/div/div/div/div/div[2]/div[1]/div/div'
+        validationText = driver.find_element(by=By.XPATH,value='//*[@id="auth0-lock-container-1"]/div/div[2]/form/div/div/div/div/div[2]/div[1]/div/div/span/span'.getText())
+        assert validationText == text
+    except TimeoutException:
+        return False
+
+
+def upload_CV_prefilled_disabled():
+    driver.find_element(By.CSS_SELECTOR,
+                        ".Wrapper-sc-9s5zbo-1:nth-child(2) .Switcheroo-ut0ai-0").click()  # to disable the  profile pre-fill
+    driver.find_element(By.CSS_SELECTOR, "#cv > .css-130mppw").click()  # upload your cv button from profile
+    driver.find_element(By.CSS_SELECTOR, ".css-130mppw:nth-child(1)").click()  # upload your CV from pop-up
+    driver.find_element(By.ID, "cv-upload-*").send_keys(
+        "D:\\Documents\\Waqas\\Resume\\Shortlyster\\Scenario1.docx")  # '//*input[@id="cv-upload-kclxdn"]'
+    driver.find_element(By.CSS_SELECTOR, ".CloseIconButton-sc-19wgu2s-0").click()
+
+
+def upload_CV_prefilled_enabled():
+    driver.find_element(By.CSS_SELECTOR, "#cv > .css-130mppw").click()  # upload your cv button from profile
+    driver.find_element(By.CSS_SELECTOR, ".css-130mppw:nth-child(1)").click()  # upload your CV from pop-up
+    driver.find_element(By.ID, "cv-upload-*").send_keys("D:\\Documents\\Waqas\\Resume\\Shortlyster\\Scenario1.docx")
+    driver.find_element(By.CSS_SELECTOR, ".CloseIconButton-sc-19wgu2s-0").click()
+
